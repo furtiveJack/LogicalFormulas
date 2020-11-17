a92a1b18b0ed843722165dbdcfaa346edcc82ac5
furtiveJack <tanguytraina@hotmail.fr>
2020-11-17 09:52:36 +0100

    Add functions signature to Fml.hs

--- src/Data/Logic/Fml.hs
+++ src/Data/Logic/Fml.hs
@@ -51,3 +51,51 @@ prettyFormat (Imply p q) = "(" ++ prettyFormat p ++ " => "  ++ prettyFormat q ++
 prettyFormat (Equiv p q) = "(" ++ prettyFormat p ++ " <=> " ++ prettyFormat q ++ ")"
 prettyFormat (Not   p)   = "-" ++ prettyFormat p
 prettyFormat (Final v)   = show v
+
+-- |’vars’ @p@ returns all variables that occur in formula @p@. Duplicate
+-- occurrences are removes.
+--vars :: (Eq a) => Fml a -> [Var.Var a]
+
+-- |’depth’ @p@ return the depth of fomula @p@.
+--depth :: (Num b, Ord b) => Fml a -> b
+
+-- |’toNNF’ @f@ converts the formula @f@ to NNF.
+--toNNF :: Fml a -> Fml a
+
+-- |’toCNF’ @f@ converts the formula @f@ to CNF.
+--toCNF :: Fml a -> Fml a
+
+-- |’toDNF’ @f@ converts the formula @f@ to DNF.
+--toDNF :: Fml a -> Fml a
+
+-- |’isNNF’ @f@ returns true iff formula @f@ is NNF.
+--isNNF :: Fml a -> Fml a
+
+-- |’isCNF’ @f@ returns true iff formula @f@ is CNF.
+--isCNF :: Fml a -> Fml a
+
+-- |’isDNF’ @f@ returns true iff formula @f@ is DNF.
+--isDNF :: Fml a -> Fml a
+
+-- |’toUniversalNAnd’ @p@ returns a NAND-formula that is equivalent
+-- to formula @p@.
+--toUniversalNAnd :: Fml a -> Fml a
+
+-- |’toUniversalNOr’ @p@ returns a NOR-formula that is equivalent
+-- to formula @p@.
+--toUniversalNOr :: Fml a -> Fml a
+
+-- |’isUniversalNAnd’ @p@ returns true iff formula @p@ uses only NAND
+-- and variables.
+--isUniversalNAnd :: Fml a -> Bool
+
+-- |’isUniversalNOr’ @p@ returns true iff formula @p@ uses only NOR
+-- and variables.
+--isUniversalNOr :: Fml a -> Bool
+
+-- |’toCCNF’ @f@ converts the formula @f@ to CCNF.
+--toCCNF :: Fml a -> Fml a
+
+-- |’isCCNF’ @f@ returns true iff formula @f@ is CCNF.
+--isCCNF :: Fml a -> Bool
+
